55107f0b4dcf91f0cd187a8b7f88358e50c01cd1
Sameer Nadeem <116316998+SameerNadeem@users.noreply.github.com>
2024-10-02 17:47:38 -0700

    Add query parameter filtering to /users route

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,67 @@
+// backend.js
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspiring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Function to find users by name
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// /users route with optional query parameter 'name'
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
